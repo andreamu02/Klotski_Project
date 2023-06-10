cec5b05d7dcffdc420db4dbefd33457c7b833c62
andreamu02 <118268232+andreamu02@users.noreply.github.com>
2023-06-10 12:45:58 +0200

    Last changes

---
+++ Klotski/Klotski/out/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.klotski.coding"},{"l":"org.klotski.controller"},{"l":"org.klotski.graphics"}];updateSearchResults();
